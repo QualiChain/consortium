13130ed822a321b2ab4626a42f3b046ede6ed97c
Rafael Belchior <rafael.belchior@tecnico.ulisboa.pt>
2020-03-10 20:37:13 +0000

    minor tweaks

--- QualiChain Higher Education Module/createAccount_script.js
+++ QualiChain Higher Education Module/createAccount_script.js
@@ -1,13 +1,14 @@
 const fs = require('fs');
 const Web3 = require('web3');
 const web3 = new Web3('https://ropsten.infura.io/v3/66a470c1158f441cac9c502cd63d4b9b');
+const result = web3.eth.accounts.create();
+const pk = result.privateKey;
+console.log('Address: ' + result.address);
+console.log('Private Key: ' + pk);
 
-console.log(web3.eth.accounts.create());
-
-
-/* web3.eth.accounts.wallet.create();
-web3.eth.accounts.wallet.add('0x26020431000baecb5e0ae79cc2fca00a8c4ce6e299889dcaa3a12b469383f2b5');
+ web3.eth.accounts.wallet.create();
+web3.eth.accounts.wallet.add(pk);
 
 var encWallet = web3.eth.accounts.wallet.encrypt("ist_qualichain");
 console.log(encWallet);
-fs.writeFileSync("keystore", JSON.stringify(encWallet)); */
+fs.writeFileSync("keystore", JSON.stringify(encWallet));
